f85c55ec3357840d954450984f02e592038907e9
NLHolann <nolann.sugay@lhoopa.com>
2025-03-31 16:49:06 +0800

    Fixed LoginModelGirl missing in the Login and Register Page

--- frontend/src/components/LoginModelGirl.jsx
+++ frontend/src/components/LoginModelGirl.jsx
@@ -3,7 +3,7 @@ const LoginModelGirl = ({ title, subtitle }) => {
       <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
         <div className="max-w-md text-center">
           <div className="mb-8 animate-pulse">
-            <img src="../public/log-in-girl.svg" alt="Login Model Girl" />
+            <img src="../log-in-girl.svg" alt="Login Model Girl" />
           </div>
           <h2 className="text-2xl font-bold mb-4">{title}</h2>
           <p className="text-base-content/60">{subtitle}</p>
